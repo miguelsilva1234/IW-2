e7dcdec6963cf1a98a3f17304c276c641bc2d8f5
miguelsilva1234 <161628496+miguelsilva1234@users.noreply.github.com>
2025-04-23 21:47:57 -0300

    amassei nos codigos

---
+++ Aula 07 IW/script01.js
@@ -0,0 +1,8 @@
+let titulo = document.getElementById("titulo");
+let imagem = document.getElementById("imagem");
+
+titulo.innerText = "Olá, DOM!"
+
+titulo.style.color = "red";
+
+imagem.src = "img/macaco.jpg";
